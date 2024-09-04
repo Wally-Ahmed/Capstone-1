3c73727550bcedc83280be53acb29ad7fe8b3aad
Wally Rakin <your_email@example.com>
2024-09-04 16:49:47 -0400

    Removing build folder that was added accidentally

--- backend/dist/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.port = exports.dbStr = exports.secretKey = void 0;
-exports.secretKey = 'secretKey';
-exports.dbStr = 'postgresql://test:pass@localhost/restaurant_manager';
-exports.port = 3000;
-module.exports = { secretKey: exports.secretKey, dbStr: exports.dbStr, port: exports.port };
