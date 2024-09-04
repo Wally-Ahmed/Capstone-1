a0ac364936988ec57b8f4176bb6c9d9932916c11
Wally Rakin <your_email@example.com>
2024-09-04 17:31:46 -0400

    Updated environment variable configuration.

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,6 +1,6 @@
-export const secretKey = 'secretKey';
-export const dbStr = 'postgresql://test:pass@localhost/restaurant_manager';
-export const port = 3000
+import { dbStr, secretKey, backendPort } from '../../globalConfig'
+
+export const port = backendPort
 
 
 module.exports = { secretKey, dbStr, port }
--- globalConfig.ts
+++ globalConfig.ts
@@ -1,9 +1,10 @@
-import { secretKey, dbStr } from './secrets'
+export const secretKey = process.env.SECRET_KEY || 'secretKey';
+export const dbStr = process.env.DB_CONNECTION_STRING || 'postgresql://test:pass@localhost/restaurant_manager';
+
+
 export const backendURL = 'http://localhost:3000/';
 export const adminURL = 'http://localhost:3001/';
 export const employeeURL = 'http://localhost:3002/';
 export const interfaceURL = 'http://localhost:3003/';
 
 export const backendPort = 3000;
-
-export { secretKey, dbStr }
