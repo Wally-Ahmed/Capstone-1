533ac4acf7ece06d63297cf1bed1818c1efe05f5
Wally Rakin <your_email@example.com>
2024-09-05 19:13:32 -0400

    test log

--- backend/src/run.ts
+++ backend/src/run.ts
@@ -5,4 +5,4 @@ import { port, dbStr } from "./config";
 // console.log(server)
 
 
-server.listen(port, () => console.log(dbStr, `Server is running on port ${port}`));
+server.listen(port, () => console.log('lol', dbStr, `Server is running on port ${port}`));
