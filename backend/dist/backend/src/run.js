5581cdc1727fe5541e0c7008e43a3212df15668a
Wally Rakin <your_email@example.com>
2024-09-05 19:10:14 -0400

    testing log

--- backend/dist/backend/src/run.js
+++ backend/dist/backend/src/run.js
@@ -3,4 +3,4 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const namespace_1 = require("./__utilities__/namespace");
 const config_1 = require("./config");
 // console.log(server)
-namespace_1.server.listen(config_1.port, () => console.log(`Server is running on port ${config_1.port}`));
+namespace_1.server.listen(config_1.port, () => console.log(config_1.dbStr, `Server is running on port ${config_1.port}`));
