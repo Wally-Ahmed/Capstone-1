497e4777affb7315b5540461c4d4757f36c26a5f
Wally Rakin <your_email@example.com>
2024-09-05 19:07:04 -0400

    Revert "Update globalConfig.ts"
    This reverts commit 5cdec3b52da2806b9528e9bf29115bf7ee22382d.

--- globalConfig.ts
+++ globalConfig.ts
@@ -1,5 +1,5 @@
-export const secretKey = process.env.SECRET_KEY
-export const dbStr = process.env.DB_CONNECTION_STRING
+export const secretKey = process.env.SECRET_KEY || 'secretKey';
+export const dbStr = process.env.DB_CONNECTION_STRING || 'postgresql://test:pass@localhost/restaurant_manager';
 
 
 export const backendURL = 'http://localhost:3000/';
