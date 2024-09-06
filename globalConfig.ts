4ef3aa621862336d6ef3472e18c548a5d27a6e08
Wally Rakin <your_email@example.com>
2024-09-06 01:10:27 -0400

    Update globalConfig.ts

--- globalConfig.ts
+++ globalConfig.ts
@@ -2,9 +2,9 @@ export const secretKey = process.env.SECRET_KEY || 'secretKey';
 export const dbStr = process.env.DB_CONNECTION_STRING || 'postgresql://test:pass@localhost/restaurant_manager';
 
 
-export const backendURL = 'http://localhost:3000/';
-export const adminURL = 'http://localhost:3001/';
-export const employeeURL = 'http://localhost:3002/';
-export const interfaceURL = 'http://localhost:3003/';
+export const backendURL = process.env.backendURL || 'http://localhost:3000/';
+export const adminURL = process.env.adminURL || 'http://localhost:3001/';
+export const employeeURL = process.env.employeeURL || 'http://localhost:3002/';
+export const interfaceURL = process.env.interfaceURL || 'http://localhost:3003/';
 
 export const backendPort = 3000;
