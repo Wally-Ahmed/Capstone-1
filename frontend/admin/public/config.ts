6916469146b586526f2b26d62fae13ad70153b00
Wally Rakin <your_email@example.com>
2024-09-13 09:12:07 -0400

    Update config.ts

--- frontend/admin/public/config.ts
+++ frontend/admin/public/config.ts
@@ -1,3 +1,4 @@
-import { backendURL } from '../../../globalConfig'
-
-export { backendURL };
+export const backendURL = process.env.backendURL || 'http://localhost:3000/';
+export const adminURL = process.env.adminURL || 'http://localhost:3001/';
+export const employeeURL = process.env.employeeURL || 'http://localhost:3002/';
+export const interfaceURL = process.env.interfaceURL || 'http://localhost:3003/';
