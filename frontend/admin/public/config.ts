fa34bb3629277fec664d1d5e6236659f4a4bf75d
Wally Rakin <your_email@example.com>
2024-09-13 09:21:28 -0400

    Revert "Update config.ts"
    This reverts commit 3ee3b0fd93b6ccf1fc51591415812ed65ccb02a8.

--- frontend/admin/public/config.ts
+++ frontend/admin/public/config.ts
@@ -1,4 +1,4 @@
-export const backendURL = process.env.backendURL
-export const adminURL = process.env.adminURL
-export const employeeURL = process.env.employeeURL
-export const interfaceURL = process.env.interfaceURL
+export const backendURL = process.env.backendURL || 'http://localhost:3000/';
+export const adminURL = process.env.adminURL || 'http://localhost:3001/';
+export const employeeURL = process.env.employeeURL || 'http://localhost:3002/';
+export const interfaceURL = process.env.interfaceURL || 'http://localhost:3003/';
