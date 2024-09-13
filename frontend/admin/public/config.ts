322660ba8040df0f60a091787a1b2af91ec6b631
Wally Rakin <your_email@example.com>
2024-09-13 09:28:32 -0400

    Update config.ts

--- frontend/admin/public/config.ts
+++ frontend/admin/public/config.ts
@@ -1,4 +1,4 @@
-export const backendURL = process.env.backendURL || 'http://localhost:3000/';
-export const adminURL = process.env.adminURL || 'http://localhost:3001/';
-export const employeeURL = process.env.employeeURL || 'http://localhost:3002/';
-export const interfaceURL = process.env.interfaceURL || 'http://localhost:3003/';
+export const backendURL = process.env.NEXT_PUBLIC_backendURL || 'http://localhost:3000/';
+export const adminURL = process.env.NEXT_PUBLIC_adminURL || 'http://localhost:3001/';
+export const employeeURL = process.env.NEXT_PUBLIC_employeeURL || 'http://localhost:3002/';
+export const interfaceURL = process.env.NEXT_PUBLIC_interfaceURL || 'http://localhost:3003/';
